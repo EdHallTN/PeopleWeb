1b7673d381194100d7b1c4b1933a516bb2e69e92
EdHall <rolltiger@gmail.com>
2016-09-28 08:40:20 -0500

    peopleweb Ed work

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Person> personList = new ArrayList<>();
+
+        File f = new File("people.csv");
+        Scanner scanner = new Scanner(f);
+        scanner.nextLine();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] column = line.split(",");
+            Person person = new Person(column[0], column[1], column[2], column[3], column[4], column[5]);
+            personList.add(person);
+        }
+
+
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    int offset =0;
+
+                    String offSetStr = request.queryParams("offset");
+                    if (offSetStr != null) {
+                        offset = Integer.valueOf(offSetStr);
+                    }
+                    int previous = (offset)-20;
+                    int next = (offset)+20;
+                    boolean condPrevious = false;
+                    boolean condNext = false;
+                    if (offset > 0) {
+                        condPrevious = true;
+                    }
+                    if (offset < personList.size()-20) {
+                        condNext = true;
+                    }
+
+                    HashMap m = new HashMap();
+                    ArrayList<Person> tempList = new ArrayList<>(personList.subList(offset, 20+offset));
+                    m.put("personList", tempList);
+                    m.put("previous", previous);
+                    m.put("next", next);
+                    m.put("condPrevious", condPrevious);
+                    m.put("condNext", condNext);
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.get(
+                "/person",
+                (request, response) -> {
+                    int personId = Integer.valueOf(request.queryParams("id"));
+                    Person person = personList.get(personId-1);
+                    return new ModelAndView(person, "people.html");
+                },
+                new MustacheTemplateEngine()
+
+        );
+    }
+}
+
